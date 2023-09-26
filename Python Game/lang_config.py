fad36cc7ecf7e780410809feefa36f8d4e6f51eb
JGallo <jlvdgallo@gmail.com>
2023-09-26 11:45:46 +0900

    2023/09/26 AM task 16

---
+++ Python Game/lang_config.py
@@ -0,0 +1,10 @@
+en = {}
+en.quit = "QUIT"
+en.yes = "YES"
+en.no = "NO"
+en.login = "LOGIN"
+en.logout = "LOGOUT"
+en.register = "REGISTER"
+en.username = "USERNAME"
+en.password = "PASSWORD"
+en.email = "EMAIL"
---
+++ Python Game/themes_config.py
@@ -0,0 +1,5 @@
+from ursina import color
+
+theme = {}
+theme.ui_button = color.dark_gray
+theme.ui_button_highlight = color.light_gray
